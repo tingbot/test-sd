a6f14de13f9edfd42d2a2901b932b580520f112b
Joe Rickerby <joerick@mac.com>
2016-06-24 16:43:38 +0100

    Don't install Tingbot OS, we'll start from a Tingbot OS image

--- install2.sh
+++ install2.sh
@@ -3,16 +3,9 @@ set -o xtrace
 
 cd "$(dirname "$0")"
 
-sudo apt-get update
-sudo apt-get install gdebi-core
-
-(
-    git clone http://github.com/tingbot/tingbot-os
-    cd tingbot-os/
-    make build-deb
-    sudo gdebi build/tingbot-os.deb
-)
-
 dirname=$(pwd)
 
-ln -snf "$dirname/startup" /apps/home
+sudo ln -snf "$dirname/startup" /apps/home
+sudo ln -snf "$dirname/restart" /apps/startup
+
+sudo shutdown -r now
