70a7824992db04d32b7bc1bee818d824ba296384
Joe <joerick@mac.com>
2016-07-12 10:59:07 +0000

    Fix starting the test routine

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 set -o errexit
 set -o xtrace
 
+cd "$(dirname "$0")"
+dirname=$(pwd)
+
 # update tingbot-python to get backlight support
 sudo pip install tingbot-python==0.5.0
 
+sudo ln -snf "$dirname/test_routine.tingapp" /apps/home
 sudo ln -snf "$dirname/test_routine.tingapp" /apps/startup
 
 sudo shutdown -r now
